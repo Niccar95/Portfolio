f8356b3d4820d80a8aa9708fcff0e6e502b5f47a
Niccar95 <niccar.95@outlook.com>
2023-11-25 20:42:38 +0100

    portfolio påbörjad

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import '../scss/style.scss'
+
+const mainContainer = document.getElementById("app");
+const mainTitle = document.createElement("h1");
+
+mainContainer.appendChild(mainTitle);
+
+mainTitle.innerHTML = "Nicolas Carrasco";
+
+const presentation = document.createElement("p");
+
+
