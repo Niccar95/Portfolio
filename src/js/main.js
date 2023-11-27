e906c5b9aaa5ee18a85b7a0c74a0c8d0f41cc824
Niccar95 <niccar.95@outlook.com>
2023-11-27 21:37:16 +0100

    portfolio struktur påbörjad

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,38 @@
 import '../scss/style.scss'
 
-const mainContainer = document.getElementById("app");
+
+const navigation = document.getElementById("header__navigation");
+
+const navLink = document.createElement("a");
+const navLink2 = document.createElement("a");
+const navLink3 = document.createElement("a");
+const navLink4 = document.createElement("a");
+
+navigation.appendChild(navLink);
+navigation.appendChild(navLink2);
+navigation.appendChild(navLink3);
+navigation.appendChild(navLink4);
+
+navLink.setAttribute("href", "#");
+navLink2.setAttribute("href", "#");
+navLink3.setAttribute("href", "#");
+navLink4.setAttribute("href", "#");
+
+navLink.textContent = "Home";
+navLink2.textContent = "Portfolio";
+navLink3.textContent = "About";
+navLink4.textContent = "Contact";
+
+const mainContainer = document.getElementById("mainContainer");
+const presentationContainer = document.getElementById("mainContainer__presentation");
 const mainTitle = document.createElement("h1");
 
-mainContainer.appendChild(mainTitle);
+presentationContainer.appendChild(mainTitle);
 
-mainTitle.innerHTML = "Nicolas Carrasco";
+mainTitle.textContent = "I'm Nicolas Carrasco";
 
 const presentation = document.createElement("p");
+presentationContainer.appendChild(presentation);
+presentation.textContent = "I'm an aspiring Frontend developer...";
 
 
